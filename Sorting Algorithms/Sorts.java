8bddf20a60445a424673f4df17539d2a892c00b9
Adam-Anthony <adam.a.pacheco@gmail.com>
2022-03-15 15:21:59 -0400

    Added Radix Sort, made clearer distinction for sort printouts.

--- Sorting Algorithms/Sorts.java
+++ Sorting Algorithms/Sorts.java
@@ -1,10 +1,18 @@
 class Sorts {
     public static void main(String[] args) {
         System.out.println("Hey world; What's up?");
-        int[] test = {1,5,9,4,4,3,6,7};
-        ShowArray(test);
-        Bubble(test);
-        ShowArray(test);
+        int[] test1 = {1,5,9,4,4,3,6,7};
+        System.out.println("Bubble");
+        ShowArray(test1);
+        int[] result1 = Bubble(test1);
+        ShowArray(test1);
+        ShowArray(result1);
+        System.out.println("Radix");
+        int[] test2 = {1,5,9,4,4,3,6,7};
+        ShowArray(test2);
+        int[] result2 = Radix(test2);
+        ShowArray(test2);
+        ShowArray(result2);
     }
 
     public static int[] Bubble(int[] arr){
@@ -21,7 +29,19 @@ class Sorts {
         return arr;
     } 
     public static int[] Radix(int[] arr){
-        return arr;
+        int [] count = {0,0,0,0,0,0,0,0,0,0};
+        for (int i=0;i<arr.length;i++){
+            count[arr[i]]++;
+        }
+        for (int i=1; i<count.length;i++){
+            count[i] += count[i-1];
+        }
+        int [] fixedArr = new int[arr.length];
+        for (int i=0; i<arr.length; i++){
+            count[arr[i]]--;
+            fixedArr[count[arr[i]]] = arr[i];
+        }
+        return fixedArr;
     }
     public static int[] Quick(int[] arr){
         return arr;
